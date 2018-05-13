6c853e585e88f9df87843e06c12054bb0cb4bfa3
mylp <mylp@dwarvesv.com>
2018-05-13 16:38:18 +0700

    updated

--- new-feature1.js
+++ new-feature1.js
@@ -1,6 +1,5 @@
 new feature 1
 new feature 1 2828 
-new feature 1 CSSStyleDeclarationnew feature 1
 
 
 
@@ -16,3 +15,21 @@ new feature 1 29292
 16126627383838393939
 838388383338838
 22222222222222228
+new feature 1 29292
+
+
+
+
+
+
+
+
+
+
+
+
+
+dkdkdksnsn
+
+diddkkdkd
+kskssk
